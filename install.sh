d820f213946d2e507272f1fa8cd86e7afe046233
José Pereira <jmfp-b@live.com.pt>
2020-03-06 02:43:40 +0000

    Testing arguments on install script

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ SERVICES=(
 )
 
 function install_base_packages() {
+	echo -e "\n\e[92mInstalling base packages..."
 	sudo pacman -S --noconfirm base-devel pkgfile binutils fakeroot git make
 }
 
 function install_yay() {
+	echo -e "\n\e[92mInstalling Yay..."
 	[ -f "$(pwd)/_yay" ] && rm -rf _yay
 	git clone https://aur.archlinux.org/yay.git _yay
 	cd _yay && makepkg -si --noconfirm
@@ -17,10 +19,12 @@ function install_yay() {
 }
 
 function install_packages() {
-	yay -S --needed `cat ./.install/packages`
+	echo -e "\n\e[92mInstalling needed packages..."
+	yay -S --needed --nocleanmenu --nodiffmenu --noeditmenu --noprovides `cat ./.install/packages`
 }
 
 function install_compositor() {
+	echo -e "\n\e[92mInstalling compositor..."
 	# Installing the compton fork with kawase blur effect
 	[ -f "$(pwd)/_compton" ] && rm -rf _compton
 	git clone https://github.com/tryone144/compton.git _compton
@@ -29,6 +33,7 @@ function install_compositor() {
 }
 
 function link_config() {
+	echo -e "\n\e[92mLinking configuration files..."
 	COMMAND='stow --adopt --target=$HOME -R `ls -d */`'
 	eval "$COMMAND -nv"
 
@@ -42,10 +47,12 @@ function link_config() {
 }
 
 function install_oh_my_zsh() {
+	echo -e "\n\e[92mInstaling Oh My Zsh..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 function enable_services() {
+	echo -e "\n\e[92mEnabling services..."
 	for i in "${SERVICES[@]}"; do
 		sudo systemctl enable "$i"
 	done
@@ -57,6 +64,7 @@ function set_shell() {
 
 function configure_lightdm() {
 	# activate the greeter
+	echo -e "\n\e[92mConfiguring LightDM..."
 	sudo sed -i -i '/greeter-session/c\greeter-session=lightdm-gtk-greeter' /etc/lightdm/lightdm.conf
 }
 
@@ -65,11 +73,49 @@ function _install() {
 	install_yay && \
 	install_packages && \
 	install_compositor && \
-	install_oh_my_zsh && \
 	link_config && \
-	set_shell && \
 	configure_lightdm && \
-	enable_services
+	enable_services && \
+	install_oh_my_zsh
 }
 
-_install
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-b|--base)
+		install_base_packages
+		shift
+		shift
+		;;
+		-y|--yay)
+		install_yay
+		shift
+		shift
+		;;
+		-p|--packages)
+		install_packages
+		shift
+		shift
+		;;
+		-c|--install-compositor)
+		install_compositor
+		shift
+		shift
+		;;
+		-l|--link-config)
+		link_config
+		shift
+		shift
+		;;
+		-e|--enable-services)
+		enable_services
+		shift
+		shift
+		;;
+		*)
+		_install
+		shift
+		;;
+	esac
+done
+set -- "${POSITIONAL[@]}" 
